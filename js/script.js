e88e7e9001ff225e18eca890b2d5a9ae3994c5d5
Neud Tesfay <kingneud55@gmail.com>
2019-04-06 20:05:18 +0800

    100tefl website update: Services page content added, Phone hamburger menu designed and implemented

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ function pauseVideo(e)
     document.getElementById("vid_intro").pause();
 }
 
+function toggleDropdown(e)
+{
+//    function $('input[type=checkbox]').click();
+    
+    var checkBoxes = $("input[type=checkbox]");
+    checkBoxes.prop("checked", !checkBoxes.prop("checked"));
+}
+
+
 $(document).ready(function() {    
     /*  STICKY NAV  */
     $('.js-section--news').waypoint(function(direction) {
